a176e0f8faca5dfcf065cd6c7de7c122da1697dd
绵绵 <yh.lwy@qq.com>
2020-04-24 18:49:43 +0800

    搭建管理员界面（修复）

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,12 +1,11 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "element-ui/lib/theme-chalk/index.css";
-import router from "./router";
-import "./plugins/element.js";
+import Vue from 'vue'
+import App from './App.vue'
+import './plugins/element.js'
+import router from './router'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
